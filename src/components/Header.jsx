f802fa80eb4f70a70c43b2928b771db609b640eb
AlexanderK64 <san-sanyth@inbox.ru>
2024-02-28 20:55:02 +0400

    start
    getting started, removing unnecessary and initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div> <span className="logo">Неоновые вывески</span></div>
+            <div className="presentation"></div>
+        </header>
+    );
+}
