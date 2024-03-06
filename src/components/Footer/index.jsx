57a9c239cd383ec08f46897eac562c6e5cdd8b58
AlexanderK64 <san-sanyth@inbox.ru>
2024-03-06 20:58:25 +0400

    footer and header
    Add footer and header separate block

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Footer.module.scss"
+
+export default function footer(){
+    return(
+        <footer>Все права на месте</footer>
+    );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Header.module.scss"
+
+export default function Header(){
+    return(
+        <header>
+            <div> <span className={styles.logo}>Неоновые вывески</span></div>
+            <div className={styles.presentation}></div>
+        </header>
+    );
+}
