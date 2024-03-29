c229e8348c4fb70b9eca88122590c807319c3196
AlexanderK64 <san-sanyth@inbox.ru>
2024-03-29 20:42:45 +0400

    add json

--- src/useAppContext.tsx
+++ src/useAppContext.tsx
@@ -1,4 +1,5 @@
 import React,{useState,useEffect,useContext} from "react";
+import axios from "axios";
 
 const AppContext=React.createContext<any>(null);
 
@@ -13,27 +14,24 @@ export const useAppContext = () =>{
 
 const AppProvider:React.FC<{children:React.ReactNode}> = ({children}) =>{
 
-    const [items,setItems]=useState<any[]>([
-        {id:1, title:'вывеска 1',img:'1.png',desc:' описание 1',category:'one',price:'100'},
-        {id:2, title:'вывеска 2',img:'2.png',desc:' описание 2',category:'текстовые',price:'200'},
-        {id:3, title:'вывеска 3',img:'3.png',desc:' описание 3',category:'текстовые',price:'300'},
-        {id:4, title:'вывеска 4',img:'4.png',desc:' описание 4',category:'текстовые',price:'400'},
-        {id:5, title:'вывеска 5',img:'5.png',desc:' описание 5',category:'five',price:'500'},
-        {id:6, title:'вывеска 6',img:'6.png',desc:' описание 6',category:'текстовые',price:'600'},
-        {id:7, title:'вывеска 7',img:'7.png',desc:' описание 7',category:'текстовые',price:'700'},
-        {id:8, title:'вывеска 8',img:'8.png',desc:' описание 8',category:'текстовые',price:'800'},
-        {id:9, title:'вывеска 9',img:'9.png',desc:' описание 9',category:'nine',price:'900'},
-        {id:10, title:'вывеска 10',img:'10.png',desc:' описание 10',category:'текстовые',price:'1000'},
-      ]);
-    
-      const [orders,setOrders]=useState<any[]>([]);
-      const [currentItems,setCurrentItems]=useState<any[]>([]);
-      const [showFullItem,setShowFullItem]=useState<boolean>(false);
-      const [fullItem,setFullItem]=useState<any>({});
+    const [items,setItems]=useState<any[]>([]);
+    const [orders,setOrders]=useState<any[]>([]);
+    const [currentItems,setCurrentItems]=useState<any[]>([]);
+    const [showFullItem,setShowFullItem]=useState<boolean>(false);
+    const [fullItem,setFullItem]=useState<any>({});
     
       useEffect(()=>{
-        setCurrentItems(items);
-      },[items]);
+        axios
+        .get("http://localhost:3001/items")
+        .then((responce)=>{
+          setItems(responce.data);
+          chooseCategory("all");
+          setCurrentItems(responce.data);
+        })
+        .catch((error)=>{
+          console.log("Ошибка при загрузке данных", error);
+        });
+      },[]);
     
     const deleteOrder = (id:number) =>{
       setOrders(orders.filter((el)=> el.id!==id));
